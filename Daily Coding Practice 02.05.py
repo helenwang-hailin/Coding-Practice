fa1dc5faa8b722416cba62efc9302015fb01a1d0
Helen Wang <hailinlin2007@gmail.com>
2025-02-05 21:59:47 -0500

    Daily Coding Practice 02.05.py

--- Daily Coding Practice 02.05.py
+++ Daily Coding Practice 02.05.py
@@ -1,3 +1,21 @@
+"""Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'"""
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -13,7 +31,7 @@ def serialize(root):
     
     return helper(root)
 
-def deserialize(data):
+def deserialize(s):
     #Decodes your encoded data to tree.
     def helper(values):
         val = next(values)
@@ -24,7 +42,7 @@ def deserialize(data):
         node.right = helper(values)
         return node
     
-    return helper(iter(data.split(',')))
+    return helper(iter(s.split(',')))
 
 # Test case
 node = Node('root', Node('left', Node('left.left')), Node('right'))
